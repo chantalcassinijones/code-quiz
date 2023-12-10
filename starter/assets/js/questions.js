6f5060d6239bc9d255c6cf2a8b4115227b936d12
Chantal Cassini-Jones <ccassinijones@gmail.com>
2023-12-10 22:43:06 +0000

    Feat: question array add

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,64 @@
+var questions = [
+    {
+        question: 'Commonly used data types do not include:',
+        answers: {
+          a: 'Strings',
+          b: 'Booleans',
+          c: 'Alerts',
+          d: 'Numbers'
+        },
+        correctAnswer: 'c'
+      },
+      {
+        question: 'Arrays in Javascript can be used to store ___ :',
+        answers: {
+          a: 'Numbers and Strings',
+          b: 'Other arrays',
+          c: 'Booleans',
+          d: 'All of the above'
+        },
+        correctAnswer: 'd'
+      },
+      {
+        question: 'How to stop an interval timer in Javascript?',
+        answers: {
+          a: 'clearTimer',
+          b: 'clearInterval',
+          c: 'intervalOver',
+          d: 'None of the above'
+        },
+        correctAnswer: 'c'
+      },
+      {
+        question: 'Which function is used to serialize an object into a JSON string in Javascript?',
+        answers: {
+          a: 'Stringify()',
+          b: 'Parse()',
+          c: 'Convert()',
+          d: 'None of the above'
+        },
+        correctAnswer: 'a'
+      },
+    {
+    question:'Inside which HTML element do we put the JavaScript?',
+    answers: {
+        a: '<script>',
+        b: '<head>',
+        c: '<body',
+        d: '<javascript>'
+    },
+    correctAnswer: 'a'
+},
+    {
+    question: 'Who invented JavaScript?',
+    answers: {
+      a: 'Douglas Crockford',
+      b: 'Sheryl Sandberg',
+      c: 'Brendan Eich',
+      d: 'None of the above'
+    },
+    correctAnswer: 'c'
+  },
+
+]
+   
